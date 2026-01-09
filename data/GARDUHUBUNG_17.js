cadde0144c069c6298d25637f16899c31075b2d6
wempiparubak <wempiup3bulukumba@gmail.com>
2026-01-09 15:22:43 +0800

    UP3 BULUKUMBA TEBANG TUNTAS 2026

---
+++ data/GARDUHUBUNG_17.js
@@ -0,0 +1,17 @@
+var json_GARDUHUBUNG_17 = {
+"type": "FeatureCollection",
+"name": "GARDUHUBUNG_17",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "name": "GH TANETE", "folders": "GARDU HUBUNG", "description": "", "altitude": 0.0, "alt_mode": "", "time_begin": "", "time_end": "", "time_when": "" }, "geometry": { "type": "Point", "coordinates": [ 120.135907173803602, -5.33115586806483 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "name": "GH SINJAI", "folders": "GARDU HUBUNG", "description": "", "altitude": 0.0, "alt_mode": "", "time_begin": "", "time_end": "", "time_when": "" }, "geometry": { "type": "Point", "coordinates": [ 120.250171424438804, -5.121033630463311 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "name": "GH SELAYAR", "folders": "GARDU HUBUNG", "description": "", "altitude": 0.0, "alt_mode": "", "time_begin": "", "time_end": "", "time_when": "" }, "geometry": { "type": "Point", "coordinates": [ 120.4609, -6.117101 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "name": "GH BONTOMANAI", "folders": "GARDU HUBUNG", "description": "", "altitude": 0.0, "alt_mode": "", "time_begin": "", "time_end": "", "time_when": "" }, "geometry": { "type": "Point", "coordinates": [ 120.2042179260079, -5.444859954109396 ] } },
+{ "type": "Feature", "properties": { "fid": "5", "name": "GH KALUMPANG", "folders": "GARDU HUBUNG", "description": "", "altitude": 0.0, "alt_mode": "", "time_begin": "", "time_end": "", "time_when": "" }, "geometry": { "type": "Point", "coordinates": [ 120.358491250826603, -5.521756261810372 ] } },
+{ "type": "Feature", "properties": { "fid": "6", "name": "GH JENEPONTO", "folders": "GARDU HUBUNG", "description": "", "altitude": 0.0, "alt_mode": "", "time_begin": "", "time_end": "", "time_when": "" }, "geometry": { "type": "Point", "coordinates": [ 119.734050113299702, -5.671531972866696 ] } },
+{ "type": "Feature", "properties": { "fid": "7", "name": "GH BANTAENG", "folders": "GARDU HUBUNG", "description": "", "altitude": 0.0, "alt_mode": "", "time_begin": "", "time_end": "", "time_when": "" }, "geometry": { "type": "Point", "coordinates": [ 119.952801527491602, -5.54384753843798 ] } },
+{ "type": "Feature", "properties": { "fid": "8", "name": "GH TANGKA", "folders": "GARDU HUBUNG", "description": "", "altitude": 0.0, "alt_mode": "", "time_begin": "", "time_end": "", "time_when": "" }, "geometry": { "type": "Point", "coordinates": [ 120.012617845575505, -5.205886921710809 ] } },
+{ "type": "Feature", "properties": { "fid": "9", "name": "GH LOPI", "folders": "GARDU HUBUNG", "description": "", "altitude": 0.0, "alt_mode": "", "time_begin": "", "time_end": "", "time_when": "" }, "geometry": { "type": "Point", "coordinates": [ 120.193034941015696, -5.556300740814896 ] } },
+{ "type": "Feature", "properties": { "fid": "10", "name": "GH TOLO", "folders": "GARDU HUBUNG", "description": "", "altitude": 0.0, "alt_mode": "", "time_begin": "", "time_end": "", "time_when": "" }, "geometry": { "type": "Point", "coordinates": [ 119.803950916327693, -5.562225023760623 ] } }
+]
+}
